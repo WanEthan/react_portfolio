4dee13f684a858e0bb5d47aaba0a5ef009fb6123
Ethan Wan <qwe052061@gmail.com>
2023-12-26 15:11:52 -0800

    add some code in home components

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import "./home.css"
+import Me from "../../assets/avatar-1.svg";
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home container'>
+      <div className="intro">
+        <img src={Me} alt="" className='home_img' />
+        <h1 className="home__name">Ethan Wan</h1>
+        <span className="home__education">I am Full-Stack Software Developer</span>
+
+        <HeaderSocials/>
+
+        <a href="#contact" className="btn">Hire Me</a>
+
+        <ScrollDown/>
+      </div>
+    </section>
   )
 }
 
